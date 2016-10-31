5b36550b5a51827f651515053b0487f6c63f3b00
lynnlangit <lynnlangit@live.com>
2016-10-31 13:07:35 -0700

    added english comments for recipe stub

--- app/src/main/java/org/tkpandroid/chooseadventure_v01/MainActivity.java
+++ app/src/main/java/org/tkpandroid/chooseadventure_v01/MainActivity.java
@@ -6,10 +6,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+//  TKPAndroid Recipe
+//  Top Level intent
 //  First screen - give user 2 options(2 buttons): explore or wake up
 //  If they click on wakeUp: they get a Toast that says: "You wake up and have boring day. The end"
 //  If they click on explore: they get a Toast that says: "You approach a glowing, green bucket of ooze.
 //      Worried that you will get in trouble, you pick up the bucket."
+//
+//  Add two text boxes with your story text
+//  Add two buttons with their function names and declare them in your code
+//  Wire button instances to declared buttons
+//  Add onClick listener for each of the buttons
+//  Add conditional ('case' or 'if') to onClick
+//  Display toast message A or B depending on which button was clicked
 
 public class MainActivity extends Activity implements View.OnClickListener
 {
